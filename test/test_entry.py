4ad206debf22543334fd4629bee81906c39ff07c
Jason Green <Git@JasonGreen.Name>
2014-01-20 15:13:27 -0500

    began test_entry. fails import

--- test/test_entry.py
+++ test/test_entry.py
@@ -2,8 +2,22 @@
 
 """ Test the entry module"""
 
+import pytest
+
 import settings
 from scanner_parser import entry
+
+#from entry_testing_tools import gen_strings
+#from figure_testing_tools import StringGenerators as StrGen
+from scanner_parser.entry import Entry, InputError
+
+
+#receive lines (list of strings)
+#confirm fourth line blank
+#loop over lines to accumulate figure strings
+#concatenate figure values into account number
+
+
 
 #lines_per_entry
 #figures_per_entry
@@ -12,5 +26,4 @@ from scanner_parser import entry
 #valid_figure_characters
 #figures
 #last_line_empty
-
 
