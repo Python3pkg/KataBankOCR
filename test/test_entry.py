8260736781a2156efffe01fb77a6b4425687fd38
Jason Green <Git@JasonGreen.Name>
2014-01-19 23:46:12 -0500

    added test_entry stub

---
+++ test/test_entry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+""" Test the entry module"""
+
+import settings
+from scanner_parser import entry
+
+#lines_per_entry
+#figures_per_entry
+#figure_width
+#characters_per_figure
+#valid_figure_characters
+#figures
+#last_line_empty
+
+
