876afd1f521ab148fa72c242b028b266c4357cc7
Jason Green <Git@JasonGreen.Name>
2014-01-20 14:55:24 -0500

    defined errors in Figure. tests now pass

--- scanner_parser/figure.py
+++ scanner_parser/figure.py
@@ -2,6 +2,15 @@
 
 import settings
 
+class FigureError(Exception):
+    " Base class for exceptions in this module "
+    pass
+
+class InputError(FigureError):
+    " Exception raised for errors in the input "
+    def __init__(self,value):
+        self.value = value
+
 class Figure():
     """ A multi-character string that represents a single character """
 
@@ -9,20 +18,21 @@ class Figure():
         self.figure_string = figure_string
         self.validate_string()
         self.parse_string()
-    
+
     def validate_string(self):
         if not isinstance(self.figure_string,str):
-            raise('not a string')
+            raise(InputError('not a string'))
         valid_characters = settings.valid_figure_characters
         if not set(self.figure_string).issubset(valid_characters):
-            raise('invalid_figure_characters')
+            raise(InputError('invalid figure characters'))
         if len(self.figure_string) < settings.characters_per_figure:
-            raise('figure_string_too_short')
+            raise(InputError('figure string too short'))
         if len(self.figure_string) > settings.characters_per_figure:
-            raise('figure_string_too_long')
+            raise(InputError('figure string too long'))
 
     def parse_string(self):
         if self.figure_string in settings.figures:
             self.value = settings.figures[self.figure_string]
         else:
-            raise('unrecognized_figure')
+            raise(InputError('unrecognized figure'))
+
