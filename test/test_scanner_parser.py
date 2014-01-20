fee331de510e10dde13a3f73461ee7b52b0c3cfb
Jason Green <Git@JasonGreen.Name>
2014-01-19 23:45:08 -0500

    added test_scanner_parser stub

--- test/test_scanner_parser.py
+++ test/test_scanner_parser.py
@@ -1,5 +1,16 @@
 #!/usr/bin/env python
 
-""" Test the scanner_parser file"""
+""" Test the scanner_parser module"""
+
+import settings
+from scanner_parser import scanner_parser
+
+#lines_per_entry
+#figures_per_entry
+#figure_width
+#characters_per_figure
+#valid_figure_characters
+#figures
+#last_line_empty
 
 
