06075a488a56a9fddfb5226d299447d736dc2aef
Jason Green <Git@JasonGreen.Name>
2014-01-19 23:41:11 -0500

    figure begun

---
+++ scanner_parser/figure.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+import settings
+
+class Figure():
+    """ A multi-character string that represents a single character """
+    def __init__(self,figure_string):
+        self.figure_string = figure_string
