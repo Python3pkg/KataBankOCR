b5854511bae9ea23d0bfb39265b5020da3cee496
Jason Green <Git@JasonGreen.Name>
2014-01-19 23:42:20 -0500

    minor cosmetic fix to settings

--- settings.py
+++ settings.py
@@ -10,7 +10,7 @@ values=('0','1','2','3','4','5','6','7','8','9',)
 
 # 10 strings composed of spaces, underscores and pipes
 # that represent the characters 0 through 9
-figures = {' _ ' +
+figures = {' _ '+
            '| |'+
            '|_|':'0',
            '   '+
