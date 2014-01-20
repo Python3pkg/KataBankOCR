f5b20a8634604f99c2a395e6e2b45fa898f079e2
Jason Green <Git@JasonGreen.Name>
2014-01-20 15:09:00 -0500

    added sets to non_strings test in test_figures

--- test/test_figure.py
+++ test/test_figure.py
@@ -10,10 +10,12 @@ from figure_testing_tools import StringGenerators as StrGen
 from scanner_parser.figure import Figure, InputError
 
 def test_instantiation_with_no_argument():
+    " confirm Figure require more than zero arguments "
     with pytest.raises(TypeError):
         f = Figure()
 
 def test_instantiation_with_multiple_arguments(min=2,max=10):
+    " confirm Figure requires fewer than 2 arguments "
     args = min
     while args <= max:
         with pytest.raises(TypeError):
@@ -21,36 +23,44 @@ def test_instantiation_with_multiple_arguments(min=2,max=10):
         args += 1
     
 def test_instantiation_with_valid_string():
+    " confirm Figure instantiates with a valid string argument "
     for valid_string in settings.figures.keys():
         f = Figure(valid_string)
         assert isinstance(f,Figure)
 
 def test_instantiation_with_non_string():
-    for non_string in (1,False,True,3.14359,[],{},0,-10):
+    " confirm figure requires a string as its argument "
+    arbitrary_non_string_values = (0,1,-10,False,True,3.14359,[],{},set())
+    for non_string in arbitrary_non_string_values:
         with pytest.raises(InputError): # 'not a string'
             f = Figure(non_string)
 
 def test_instantiation_with_insufficient_string_length():
+    " confirm figure checks minimum string length "
     for short_string in gen_strings(50,StrGen.too_short):
         with pytest.raises(InputError): # 'figure string too short'
             f = Figure(short_string)
 
 def test_instantiation_with_excessive_string_length():
+    " confirm figure checks maximum string length "
     for long_string in gen_strings(50,StrGen.too_long):
         with pytest.raises(InputError): # 'figure string too long'
             f = Figure(long_string)
 
 def test_instantiation_with_adulterated_string():
+    " confirm figure checks for inappropriate characters in its input string "
     for bad_string in gen_strings(50,StrGen.adulterated):
         with pytest.raises(InputError): # 'invalid figure characters'
             f = Figure(bad_string)
 
 def test_instantiation_with_unknown_string():
+    " confirm figure refuses unknown strings "
     for unknown_string in gen_strings(50,StrGen.unknown):
         with pytest.raises(InputError): # 'unknown figure'
             f = Figure(unknown_string)
 
 def test_instantiation_with_known_string():
+    " confirm figure correctly identifies known strings "
     for known_string in gen_strings(50,StrGen.known):
         f = Figure(known_string)
         expected = settings.figures[known_string]
