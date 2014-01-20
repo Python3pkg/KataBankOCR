a6cd5890f7d5d28b9b8e1c32368295baea9f9ccd
Jason Green <Git@JasonGreen.Name>
2014-01-20 15:16:12 -0500

    began Entry. tests pass

---
+++ scanner_parser/entry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import settings
+
+class EntryError(Exception):
+    " Base class for exceptions in this module "
+    pass
+
+class InputError(EntryError):
+    " Exception raised for errors in the input "
+    def __init__(self,value):
+        self.value = value
+
+class Entry():
+    " Lines of characters containing figures that represent an account number "
+    pass
