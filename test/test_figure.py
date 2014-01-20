ec867f3fd7c80595af8a9c1a191de75ec6822ea8
Jason Green <Git@JasonGreen.Name>
2014-01-20 16:25:09 -0500

    minor: capitalized class in comments

--- test/test_figure.py
+++ test/test_figure.py
@@ -29,38 +29,38 @@ def test_instantiation_with_valid_string():
         assert isinstance(f,Figure)
 
 def test_instantiation_with_non_string():
-    " confirm figure requires a string as its argument "
+    " confirm Figure requires a string as its argument "
     arbitrary_non_string_values = (0,1,-10,False,True,3.14359,(),[],{},set())
     for non_string in arbitrary_non_string_values:
         with pytest.raises(InputError): # 'not a string'
             f = Figure(non_string)
 
 def test_instantiation_with_insufficient_string_length():
-    " confirm figure checks minimum string length "
+    " confirm Figure checks minimum string length "
     for short_string in gen_strings(50,StrGen.too_short):
         with pytest.raises(InputError): # 'figure string too short'
             f = Figure(short_string)
 
 def test_instantiation_with_excessive_string_length():
-    " confirm figure checks maximum string length "
+    " confirm Figure checks maximum string length "
     for long_string in gen_strings(50,StrGen.too_long):
         with pytest.raises(InputError): # 'figure string too long'
             f = Figure(long_string)
 
 def test_instantiation_with_adulterated_string():
-    " confirm figure checks for inappropriate characters in its input string "
+    " confirm Figure checks for inappropriate characters in its input string "
     for bad_string in gen_strings(50,StrGen.adulterated):
         with pytest.raises(InputError): # 'invalid figure characters'
             f = Figure(bad_string)
 
 def test_instantiation_with_unknown_string():
-    " confirm figure refuses unknown strings "
+    " confirm Figure refuses unknown strings "
     for unknown_string in gen_strings(50,StrGen.unknown):
         with pytest.raises(InputError): # 'unknown figure'
             f = Figure(unknown_string)
 
 def test_instantiation_with_known_string():
-    " confirm figure correctly identifies known strings "
+    " confirm Figure correctly identifies known strings "
     for known_string in gen_strings(50,StrGen.known):
         f = Figure(known_string)
         expected = settings.figures[known_string]
