8c6a38e9b22140004709d5ff18de2f0d336bce32
Jason Green <Git@JasonGreen.Name>
2014-02-08 18:23:07 -0500

    cleaned up test_stories

--- test/test_stories.py
+++ test/test_stories.py
@@ -1,4 +1,4 @@
-" Test user stories (from kata.txt) "
+"test user stories (from kata.txt)"
 
 import pytest
 import subprocess
@@ -9,7 +9,7 @@ path_to_advanced_input = "test/input_files/advanced.txt"
 class TestStoryOne:
 
     def test_story_one(self):
-        " confirm story one parses Accounts from StdIn "
+        "confirm story one parses Accounts from StdIn"
         accounts_in_basic_input = ['000000000', '111111111', '222222222', '333333333',
                                    '444444444', '555555555', '666666666', '777777777',
                                    '888888888', '999999999', '123456789']
@@ -21,7 +21,7 @@ class TestStoryOne:
 class TestStoryTwo:
 
     def test_story_two(self):
-        " confirm story two recognizes [in]valididity of Accounts "
+        "confirm story two recognizes [in]valididity of Accounts"
         valid_accounts_in_basic_input = ['000000000', '123456789',]
         invalid_accounts_in_basic_input = ['111111111', '222222222', '333333333',
                                            '444444444', '555555555', '666666666', 
@@ -34,49 +34,47 @@ class TestStoryTwo:
 
 class TestStoryThree:
 
-    def test_parser_with_basic_input(tmpdir):
-        " confirm Results from basic input file "
-        expected_results = [
-            '000000000',
-            '111111111 ERR',
-            '222222222 ERR', 
-            '333333333 ERR',
-            '444444444 ERR',
-            '555555555 ERR',
-            '666666666 ERR',
-            '777777777 ERR',
-            '888888888 ERR',
-            '999999999 ERR',
-            '123456789'
-            ]
+    basic_input_results = (
+        '000000000',
+        '111111111 ERR',
+        '222222222 ERR', 
+        '333333333 ERR',
+        '444444444 ERR',
+        '555555555 ERR',
+        '666666666 ERR',
+        '777777777 ERR',
+        '888888888 ERR',
+        '999999999 ERR',
+        '123456789'
+        )
+    advanced_input_results = (
+        '000000051', 
+        '49006771? ILL',
+        '1234?678? ILL',
+        '200000000 ERR',
+        '490067715 ERR',
+        '?23456789 ILL',
+        '0?0000051 ILL',
+        '49086771? ILL',
+        )
+    @pytest.mark.parametrize('input_path, expected_results',(
+            (path_to_basic_input, basic_input_results),
+            (path_to_advanced_input, advanced_input_results),
+            ))
+    def test_story_three(self, input_path, expected_results):
+        "confirm Results from input file"
         expected = '\n'.join(expected_results) + '\n'
-        with open(path_to_basic_input) as input_file:
+        with open(input_path) as input_file:
             found = subprocess.check_output('parser/story_three.py', stdin=input_file)
         assert expected == found
 
-    def test_parser_with_advanced_input(tmpdir):
-        " confirm Results from advanced input file "
-        expected_results = [
-            '000000051', 
-            '49006771? ILL',
-            '1234?678? ILL',
-            '200000000 ERR',
-            '490067715 ERR',
-            '?23456789 ILL',
-            '0?0000051 ILL',
-            '49086771? ILL',
-            ]
-        expected = '\n'.join(expected_results) + '\n'
-        with open(path_to_advanced_input) as input_file:
-            found = subprocess.check_output('parser/story_three.py', stdin=input_file)
-        assert expected == found
 """
 
 class TestStoryFour:
     # TODO: Implement
     
     def test_story_four_with_basic_input(tmpdir):
-        " confirm story four parses Results from basic input file "
+        "confirm story four parses Results from basic input file"
         results_in_basic_input = ['000000000', '111111111 ERR', '222222222 ERR', 
                                   '333333333 ERR', '444444444 ERR', '555555555 AMB',
                                   '666666666 AMB', '777777177', '888888888 AMB',
