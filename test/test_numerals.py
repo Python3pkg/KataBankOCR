84e07a9c2998650ced04862ec61bab44e5777e25
Jason Green <Git@JasonGreen.Name>
2014-02-08 20:16:54 -0500

    cleaned up test_numerals

--- test/test_numerals.py
+++ test/test_numerals.py
@@ -1,4 +1,4 @@
-" test the numerals module "
+"test the numerals module"
 
 import pytest
 import random
@@ -8,33 +8,31 @@ from parser.numerals import numerals_from_figures
 from parser.validators import Validate
 
 from common_tools import invalid_lengths, fit_to_length, replace_element
-from common_tools import get_unknown_figure
-from common_tools import entry_from_account
 
 class TestNumeralsFromFigures:
-    " exercise the numerals_from_figures function "
+    "exercise the numerals_from_figures function"
 
     class TestInput:
-        " confirm invalid input raises appropriate error "
+        "confirm invalid input raises appropriate error"
 
         def test_with_non_iterable(self, non_iterable):
-            " confirm error raised on non-iterable input "
+            "confirm error raised on non-iterable input"
             entries = numerals_from_figures(non_iterable)
             pytest.raises(TypeError, list, entries)
 
         def test_with_non_string(self, non_string):
-            " confirm error raised on iterable that yields a non_string "
+            "confirm error raised on iterable that yields a non_string"
             figures = [non_string,]
             entries = numerals_from_figures(figures)
             pytest.raises(TypeError, list, entries)
 
         @pytest.fixture(params=invalid_lengths(settings.strokes_per_figure))
         def invalid_figure_length(self, request):
-            " return an invalid length for a Figure "
+            "return an invalid length for a Figure"
             return request.param
 
         def test_with_figure_of_invalid_length(self, get_figure, invalid_figure_length):
-            " confirm error raised for figure of invalid length "
+            "confirm error raised for figure of invalid length"
             invalid_length_figure = fit_to_length(get_figure(), invalid_figure_length)
             entries = numerals_from_figures([invalid_length_figure,])
             pytest.raises(ValueError, list, entries)
@@ -46,7 +44,7 @@ class TestNumeralsFromFigures:
 
         @pytest.mark.parametrize('non_stroke', some_non_strokes)
         def test_with_figure_containing_invalid_stroke(self, get_figure, non_stroke):
-            " confirm error raised for figure containing an invalid stroke "
+            "confirm error raised for figure containing an invalid stroke"
             adulterated_figure = replace_element(get_figure(), non_stroke)
             entries = numerals_from_figures([adulterated_figure,])
             e = pytest.raises(TypeError, list, entries)
@@ -55,31 +53,26 @@ class TestNumeralsFromFigures:
             assert msg in e.value.message
 
     class TestOutput:
-        " confirm valid input results in valid output "
+        "confirm valid input results in valid output"
 
-        @pytest.fixture
-        def figures(self, get_figure):
-            " return an entry's worth of random figures "
-            return [get_figure() for i in range(settings.figures_per_entry)]
-
-        def test_returns_iterable(self, figures):
-            " confirm iterable "
-            numerals = numerals_from_figures(figures)
+        def test_returns_iterable(self, get_figures):
+            "confirm iterable"
+            numerals = numerals_from_figures(get_figures())
             Validate.iterable(numerals)
 
-        def test_element_types(self, figures):
-            " confirm iterable yields strings "
-            numerals = numerals_from_figures(figures)
-            Validate.elements('type', basestring, figures, 'Numeral')
+        def test_element_types(self, get_figures):
+            "confirm iterable yields strings"
+            numerals = numerals_from_figures(get_figures())
+            Validate.elements('type', basestring, numerals, 'Numeral')
 
-        def test_element_lengths(self, figures):
-            " confirm iterable yields elements of correct length "
-            numerals = numerals_from_figures(figures)
+        def test_element_lengths(self, get_figures):
+            "confirm iterable yields elements of correct length"
+            numerals = numerals_from_figures(get_figures())
             expected_length = 1
             Validate.elements('length', expected_length, numerals, 'Numeral')
 
         def test_parses_known_figures_to_numerals(self):
-            " confirm known figures recognized correctly "
+            "confirm known figures recognized correctly"
             figures, numerals = zip(*settings.figures.items())
             expected = numerals
             found = tuple(numerals_from_figures(figures))
