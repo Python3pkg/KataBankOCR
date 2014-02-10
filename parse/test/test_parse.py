c7002fd85a4770fb158f916f76f1733ecb46e310
Jason Green <Git@JasonGreen.Name>
2014-02-10 00:20:08 -0500

    minor cleanup of test_parse

--- parse/test/test_parse.py
+++ parse/test/test_parse.py
@@ -2,6 +2,7 @@
 
 import pytest
 import subprocess
+from functools import partial
 
 from test_input import Basic, Advanced
 
@@ -29,8 +30,7 @@ def test_in_path_and_stdout(tmpdir, expectations_and_source):
 def test_in_path_and_out_path(tmpdir, expectations_and_source):
     "confirm Results parsed correctly from in_path to out_path"
     expected_results, input_path = expectations_and_source
-    output_path = 'tmp_out_file'
-    output_path = str(tmpdir.join(output_path))
+    output_path = str(tmpdir.join('tmp_out_file'))
     subprocess.call(['parse/parse', input_path, output_path])
     with open(output_path) as parsed_results:
         found_results = parsed_results.read()
@@ -39,8 +39,7 @@ def test_in_path_and_out_path(tmpdir, expectations_and_source):
 def test_stdin_and_out_path(tmpdir, expectations_and_source):
     "confirm Results parsed correctly from StdIn to out_path"
     expected_results, input_path = expectations_and_source
-    output_path = 'tmp_out_file'
-    output_path = str(tmpdir.join(output_path))
+    output_path = str(tmpdir.join('tmp_out_file'))
     with open(input_path) as input_file:
         subprocess.call(['parse/parse', '-', output_path], stdin=input_file)
     with open(output_path) as parsed_results:
