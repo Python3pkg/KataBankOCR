a2b5d48e0a3315e945fac3845dbe04213e9bcc57
Jason Green <Git@JasonGreen.Name>
2014-02-11 02:00:19 -0500

    check_function test creator looks good

--- parse/test/check_function.py
+++ parse/test/check_function.py
@@ -1,34 +1,43 @@
 "functions that return tests showing other funcs validate their input"
 
 import pytest
+from common_tools import replace_element
 
 def raises_on_non_iterable(function):
     "return test of error raised on non-iterable input"
     some_non_iterables=(0, 1, -10, False, True, 3.14159)
     def non_iterable_test():
         for value in some_non_iterables:
-            result = function(value)
-            pytest.raises(TypeError, list, result)
+            iterator = function(value)
+            pytest.raises(TypeError, list, iterator)
     return non_iterable_test
 
 def raises_on_bad_element_type(function, valid_element):
-    "return test of error raised on iterable that yields a non_string"
+    "return test of error raised on element of unexpected type"
     def element_type_test():
         for bad_element in _get_values_of_different_type(valid_element):
-            iterable = [bad_element]
-            result = function(iterable)
-            pytest.raises(TypeError, list, result)
+            iterator = function([bad_element])
+            pytest.raises(TypeError, list, iterator)
     return element_type_test
 
 def raises_on_bad_element_length(function, valid_element):
-    "confirm error raised on iterable yielding unexpected length element"
+    "return test of error raised on element of unexpected length"
     valid_length = len(valid_element)
     def element_length_test():
         for invalid_length in _get_invalid_lengths(valid_length):
             invalid_length_element = _fit_to_length(valid_element, invalid_length)
-            results = function([invalid_length_element])
-            pytest.raises(ValueError, list, results)
+            iterator = function([invalid_length_element])
+            pytest.raises(ValueError, list, iterator)
     return element_length_test
+
+def raises_on_bad_element_composition(function, valid_element, adulterants):
+    "return test of error raised when element contains invalid element"
+    def element_composition_test():
+        for adulterant in adulterants:
+            adulterated_element = replace_element(valid_element, adulterant)
+            iterator = function([adulterated_element])
+            pytest.raises(TypeError, list, iterator)
+    return element_composition_test
 
 def _get_values_of_different_type(obj):
     "Return an arbitrary non-string value"
--- parse/test/test_accounts.py
+++ parse/test/test_accounts.py
@@ -9,35 +9,21 @@ from parse.accounts import accounts_from_numerals
 from common_tools import flatten, replace_element
 import check_function
 
-func = accounts_from_numerals
+function = accounts_from_numerals
 valid_numeral = random.choice(list(settings.valid_numerals))
+adulterants = ['\t', '-', 'I', 'l', '/', '\\', '\r']
 
-test_iterability = check_function.raises_on_non_iterable(func)
-test_non_string = check_function.raises_on_bad_element_type(func, valid_numeral)
-test_element_length = check_function.raises_on_bad_element_length(func, valid_numeral)
+test_iterability = check_function.raises_on_non_iterable(function)
+test_non_string = check_function.raises_on_bad_element_type(function, valid_numeral)
+test_element_length = check_function.raises_on_bad_element_length(function, valid_numeral)
+test_element_composition = check_function.raises_on_bad_element_composition(function, 
+                                                                            valid_numeral,
+                                                                            adulterants)
 
-class TestAccountsFromNumerals:
-    "exercise the accounts_from_numerals function"
-
-    class TestInput:
-        "confirm invalid input raises appropriate error"
-
-        def test_with_non_numeral(self, get_account, non_numeral):
-            "confirm error raised for non_numeral"
-            adulterated_account = replace_element(get_account(), non_numeral)
-            accounts = accounts_from_numerals(adulterated_account)
-            e = pytest.raises(TypeError, list, accounts)
-            message = 'Numeral "%s" contains unexpected element "%s" at index 0'
-            message = message % (non_numeral, non_numeral)
-            assert message == e.value.message
-
-    class TestOutput:
-        "confirm valid input results in valid output"
-
-        def test_parses_known_numerals_to_accounts(self, get_accounts):
-            "confirm known numerals recognized correctly"
-            accounts = get_accounts()
-            expected = list(accounts)
-            numerals = flatten(accounts)
-            found = list(accounts_from_numerals(numerals))
-            assert expected == found
+def test_parses_known_numerals_to_accounts(get_accounts):
+    "confirm known numerals recognized correctly"
+    accounts = get_accounts()
+    expected = list(accounts)
+    numerals = flatten(accounts)
+    found = list(accounts_from_numerals(numerals))
+    assert expected == found
