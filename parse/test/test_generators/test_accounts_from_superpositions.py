a3855e4a3438a32e4fc98ad41067699a59101c64
Jason Green <Git@JasonGreen.Name>
2014-02-16 12:01:37 -0500

    more cleanup of test_accounts_from_superpositions

--- parse/test/test_generators/test_accounts_from_superpositions.py
+++ parse/test/test_generators/test_accounts_from_superpositions.py
@@ -5,24 +5,18 @@ import pytest
 from parse.generators.accounts_from_superpositions import accounts_from_superpositions
 
 import check_generator
-import fixtures
+from fixtures import Accounts, Superpositions
 
 test_iterability = check_generator.raises_on_non_iterable(generator=accounts_from_superpositions)
 test_element_type = \
     check_generator.raises_on_bad_element_type(generator=accounts_from_superpositions,
                                                value_or_type=dict)
 
-accounts = (
-    fixtures.Accounts.of_example_accounts()
-    + fixtures.Accounts.of_flawed_accounts()
-    )
-superpositions = (
-    fixtures.Superpositions.of_example_accounts()
-    + fixtures.Superpositions.of_flawed_accounts()
-    )
+accounts = Accounts.of_example_accounts() + Accounts.of_flawed_accounts()
+superpositions = Superpositions.of_example_accounts() + Superpositions.of_flawed_accounts()
 
 @pytest.mark.parametrize('expected_account, superpositions', (zip(accounts, superpositions)))
-def test_parses_superpositions_to_expected_accounts(expected_account, superpositions):
+def test_known_superpositions_yield_expected_account(expected_account, superpositions):
     "confirm known superpositions yield expected accounts"
     found_accounts = list(accounts_from_superpositions(superpositions))
     assert [expected_account] == found_accounts
