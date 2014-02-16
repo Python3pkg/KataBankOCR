ef1f176c4967b6dde8c5e9451babead584269a0c
Jason Green <Git@JasonGreen.Name>
2014-02-16 18:47:36 -0500

    more clean up of test_results_from_accounts

--- parse/test/test_generators/test_results_from_accounts.py
+++ parse/test/test_generators/test_results_from_accounts.py
@@ -1,5 +1,7 @@
 "test the results_from_accounts generator"
 
+import pytest
+
 from parse.generators.results_from_accounts import results_from_accounts
 
 import check_generator
@@ -21,17 +23,12 @@ test_element_composition = check_generator.raises_on_bad_element_composition(
     adulterants=fixtures.ArbitraryValues.invalid_numerals()
     )
 
-def test_parses_known_accounts_to_results():
+@pytest.mark.parametrize('expected_results, accounts', (
+        (fixtures.Results.of_example_accounts(), fixtures.Accounts.of_example_accounts()),
+        (fixtures.Results.of_flawed_accounts(), fixtures.Accounts.of_flawed_accounts()),
+        ))
+def test_parses_known_accounts_to_results(expected_results, accounts):
     "confirm known account recognized correctly"
-    expected_results = fixtures.Results.of_example_accounts()
-    iterator = results_from_accounts(fixtures.Accounts.of_example_accounts())
+    iterator = results_from_accounts(accounts)
     found_results = list(iterator)
     assert expected_results == found_results
-
-def test_parses_account_with_errors_to_expected_result():
-    "confirm superpositions with flawed figures yield expected accounts"
-    expected_results = fixtures.Results.of_flawed_accounts()
-    iterator = results_from_accounts(fixtures.Accounts.of_flawed_accounts())
-    found_results = list(iterator)
-    assert expected_results == found_results
-
