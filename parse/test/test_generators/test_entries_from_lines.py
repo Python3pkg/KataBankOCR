7ce16284aa1a8f027ecadaf70db86d45c92dd401
Jason Green <Git@JasonGreen.Name>
2014-02-16 12:08:18 -0500

    cleaned up test_entries_from_lines

--- parse/test/test_generators/test_entries_from_lines.py
+++ parse/test/test_generators/test_entries_from_lines.py
@@ -5,22 +5,21 @@ import pytest
 from parse.generators.entries_from_lines import entries_from_lines
 
 import check_generator
-import fixtures
+from fixtures import Entries, Lines
 
 test_iterability = check_generator.raises_on_non_iterable(generator=entries_from_lines)
 
 def test_known_lines_grouped_to_known_entries():
     "confirm known lines grouped into known entries"
-    expected = fixtures.Entries.of_basic_input_file()
-    lines = fixtures.Lines.of_basic_input_file()
-    found = entries_from_lines(lines)
-    assert expected == list(found)
+    expected_entries = Entries.of_basic_input_file()
+    lines = Lines.of_basic_input_file()
+    found_entries = list(entries_from_lines(lines))
+    assert expected_entries == found_entries
 
 def test_insufficient_line_count_raises_expected_error():
     "confirm a truncated list of lines raises appropriate error"
-    expected = fixtures.Entries.of_basic_input_file()
-    lines = fixtures.Lines.of_basic_input_file()
-    lines = lines[:-1]
-    entries = entries_from_lines(lines)
-    pytest.raises(ValueError, list, entries)
+    lines = Lines.of_basic_input_file()
+    truncated_lines = lines[:-1]
+    iterator = entries_from_lines(truncated_lines)
+    pytest.raises(ValueError, list, iterator)
     message = 'File ended mid-entry'
