b81e11e0ddabb0db136baa68c28ec61297b086d0
Jason Green <Git@JasonGreen.Name>
2014-02-16 12:13:30 -0500

    fixed test_entries_from_lines to handle single-line entries

--- parse/test/test_generators/test_entries_from_lines.py
+++ parse/test/test_generators/test_entries_from_lines.py
@@ -2,6 +2,7 @@
 
 import pytest
 
+from parse import settings
 from parse.generators.entries_from_lines import entries_from_lines
 
 import check_generator
@@ -16,10 +17,11 @@ def test_known_lines_grouped_to_known_entries():
     found_entries = list(entries_from_lines(lines))
     assert expected_entries == found_entries
 
-def test_insufficient_line_count_raises_expected_error():
-    "confirm a truncated list of lines raises appropriate error"
-    lines = Lines.of_basic_input_file()
-    truncated_lines = lines[:-1]
-    iterator = entries_from_lines(truncated_lines)
-    pytest.raises(ValueError, list, iterator)
-    message = 'File ended mid-entry'
+if settings.lines_per_entry > 1:
+    def test_insufficient_line_count_raises_expected_error():
+        "confirm a truncated list of lines raises appropriate error"
+        lines = Lines.of_basic_input_file()
+        truncated_lines = lines[:-1]
+        iterator = entries_from_lines(truncated_lines)
+        pytest.raises(ValueError, list, iterator)
+        message = 'File ended mid-entry'
