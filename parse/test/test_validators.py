aa081dad1717cd65f21f712dc54562330fef46ba
Jason Green <Git@JasonGreen.Name>
2014-02-16 21:58:29 -0500

    minor cleanup to test_validators

--- parse/test/test_validators.py
+++ parse/test/test_validators.py
@@ -4,7 +4,7 @@ import pytest
 import random
 
 from parse.validators import Validate
-import fixture_methods
+from fixture_methods import ArbitraryValues
 
 @pytest.fixture
 def name():
@@ -13,7 +13,7 @@ def name():
     return random.choice(some_arbitrary_names)
 
 class TestType():
-    "exercise the Validate.type classmethod"
+    "exercise the Validate.type method"
 
     matched_type_value_pairs = ((basestring, 'foo'), (list, []), (tuple, ()), (int, 1))
     @pytest.mark.parametrize("expected_type, value", matched_type_value_pairs)
@@ -24,7 +24,7 @@ class TestType():
     mismatched_type_value_pairs = ((int, 'A',), (tuple, [],), (basestring, (1,)))
     @pytest.mark.parametrize("expected_type, value", mismatched_type_value_pairs)
     def test_with_invalid_type(self, expected_type, value, name):
-        "confirm raises correctly on invalid type"
+        "confirm raises with expected message on invalid type"
         message = '%s "%s" of unexpected type. Expected:%s. Found:%s.'
         message = message % (name, value, expected_type, str(type(value)))
         arguments = (expected_type, value, name)
@@ -32,7 +32,7 @@ class TestType():
         assert message == e.value.message
 
 class TestLength():
-    "exercise the Validate.length classmethod"
+    "exercise the Validate.length method"
 
     matched_length_value_pairs = ((0, ''), (0, []), (0, ()), (1, 'A'),
                                   (2, 'ab'), (2, [1, 2]), (3, (1, 2, 3)))
@@ -52,7 +52,7 @@ class TestLength():
         assert message == e.value.message
 
 class TestComposition():
-    "exercise the Validate.composition classmethod"
+    "exercise the Validate.composition method"
 
     matched_components_value_pairs = ((['a', 'b', 'c'], 'baabccab'),
                                       (['ab', 'XY', 'zz'], ('XY', 'ab', 'ab')),
@@ -66,27 +66,28 @@ class TestComposition():
 
     mismatched_components_value_index_triads = ((['a', 'b', 'c'], 'abcd', 3),
                                                 ((10, 20, 30), [1, 2], 0),
-                                                ([0, 1, 2, 3], (2, 4, 6), 1),)
+                                                ([0, 1, 2, 3], (2, 4, 6), 1),
+                                                (['ab', 'bc', 'de'], 'abbcde', 0),)
     @pytest.mark.parametrize("allowed_components, value, bad_index",
                              mismatched_components_value_index_triads)
     def test_with_invalid_composition(self, allowed_components, value, bad_index, name):
         "confirm raises correctly on value containing an unexpected element"
-        element = value[bad_index]
+        unexpected_element = value[bad_index]
         message = '%s "%s" contains unexpected element "%s" at index %d'
-        message = message % (name, value, element, bad_index)
+        message = message % (name, value, unexpected_element, bad_index)
         arguments = (allowed_components, value, name)
         e = pytest.raises(TypeError, Validate.composition, *arguments)
         assert message == e.value.message
 
 class TestIterable():
-    "exercise the Validate.iterable classmethod"
+    "exercise the Validate.iterable method"
 
-    @pytest.mark.parametrize("iterable", fixture_methods.ArbitraryValues.iterables())
+    @pytest.mark.parametrize("iterable", ArbitraryValues.iterables())
     def test_with_iterable(self, iterable):
         "confirm passes silently on iterable value"
         assert None == Validate.iterable(iterable)
 
-    @pytest.mark.parametrize("non_iterable", fixture_methods.ArbitraryValues.non_iterables())
+    @pytest.mark.parametrize("non_iterable", ArbitraryValues.non_iterables())
     def test_with_non_iterable(self, non_iterable):
         "confirm raises correctly on non-iterable value"
         e = pytest.raises(TypeError, Validate.iterable, non_iterable)
