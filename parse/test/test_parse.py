1fea3476f4bdc27a77df4b07f12228ada2852fbc
Jason Green <Git@JasonGreen.Name>
2014-02-16 22:49:26 -0500

    minor clean up in test_parse

--- parse/test/test_parse.py
+++ parse/test/test_parse.py
@@ -11,9 +11,9 @@ from fixture_methods import Results, Paths
         ))
 def expectations_and_source(request):
     "return expected results and input path from which to find them"
-    expected, path = request.param
-    expected = '\n'.join(expected) + '\n'
-    return expected, path
+    expected_results, path = request.param
+    expected_results = '\n'.join(expected_results) + '\n'
+    return expected_results, path
 
 def test_stdin_and_stdout(expectations_and_source):
     "confirm Results parsed correctly from StdIn to StdOut"
@@ -22,19 +22,10 @@ def test_stdin_and_stdout(expectations_and_source):
         found_results = subprocess.check_output(['parse/parse', '-'], stdin=input_file)
     assert expected_results == found_results
 
-def test_in_path_and_stdout(tmpdir, expectations_and_source):
+def test_in_path_and_stdout(expectations_and_source):
     "confirm Results parsed correctly from path to StdOut"
     expected_results, input_path = expectations_and_source
     found_results = subprocess.check_output(['parse/parse', input_path])
-    assert expected_results == found_results
-
-def test_in_path_and_out_path(tmpdir, expectations_and_source):
-    "confirm Results parsed correctly from in_path to out_path"
-    expected_results, input_path = expectations_and_source
-    output_path = str(tmpdir.join('tmp_out_file'))
-    subprocess.call(['parse/parse', input_path, output_path])
-    with open(output_path) as parsed_results:
-        found_results = parsed_results.read()
     assert expected_results == found_results
 
 def test_stdin_and_out_path(tmpdir, expectations_and_source):
@@ -43,6 +34,15 @@ def test_stdin_and_out_path(tmpdir, expectations_and_source):
     output_path = str(tmpdir.join('tmp_out_file'))
     with open(input_path) as input_file:
         subprocess.call(['parse/parse', '-', output_path], stdin=input_file)
+    with open(output_path) as parsed_results:
+        found_results = parsed_results.read()
+    assert expected_results == found_results
+
+def test_in_path_and_out_path(tmpdir, expectations_and_source):
+    "confirm Results parsed correctly from in_path to out_path"
+    expected_results, input_path = expectations_and_source
+    output_path = str(tmpdir.join('tmp_out_file'))
+    subprocess.call(['parse/parse', input_path, output_path])
     with open(output_path) as parsed_results:
         found_results = parsed_results.read()
     assert expected_results == found_results
